01ace438e8da28ff770e13feb227a64177b3ac71
Stephanie Friend <stephaniefriend91@gmail.com>
2020-05-30 00:46:41 -0600

    Add validates presence of

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :zipcode, zipcode: { country_code: :us }, presence: true
+  validates_presence_of :name, :email, :image
 
   def self.from_omniauth(auth)
     where(email: auth.info.email).first_or_initialize do |user|
