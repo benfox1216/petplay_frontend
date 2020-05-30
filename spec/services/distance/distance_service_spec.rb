e1f2b5ad260ace0237362935d10e0b6ad5a69659
Anthony Gomes <code@tonygom.es>
2020-05-30 08:16:22 -0600

    Update DistanceService Spec

--- spec/services/distance/distance_service_spec.rb
+++ spec/services/distance/distance_service_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Google Distance Service' do
-  service = DistanceService.new
+  it 'can get distance between origin & destination' do
+    service = DistanceService.new
 
-  expect(service.class).to eql(DistanceService)
-  location1 = service.by_location(origin, destination)
-  expect(location1).to have_key :distance
+    expect(service.class).to eql(DistanceService)
+    origin = { zip_code: 80202 }
+    destination = { zip_code: 80212 }
+    location1 = service.by_location(origin, destination)
+    expect(location1).to have_key :distance
+  end
 end
