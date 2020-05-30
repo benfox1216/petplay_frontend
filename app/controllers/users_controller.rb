ab3e8af634dd2f5706d03b3326dc139098064af8
Stephanie Friend <stephaniefriend91@gmail.com>
2020-05-29 23:15:05 -0600

    Update show action and add zip action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController 
 
-  def show 
-  end 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def zip
+    @user = User.find(params[:id])
+    @user.update(zipcode: user_params[:zipcode])
+    redirect_to "/profile/#{@user.id}"
+  end
+
+  def user_params
+    params.permit(:name, :email, :image, :zipcode, :github_token)
+  end
 end 
