c18a95dabfc50bcf599dea041ef0eea95ca601d1
Anthony Gomes <code@tonygom.es>
2020-05-30 08:05:15 -0600

    Change file name for readability

---
+++ spec/services/distance/distance_service_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Google Distance Service' do
+  service = DistanceService.new
+
+  expect(service.class).to eql(DistanceService)
+  location1 = service.by_location(origin, destination)
+  expect(location1).to have_key :distance
+end
