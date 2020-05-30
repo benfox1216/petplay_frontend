dd56e4617ed7a560b20500a40f346a1533f2b235
Stephanie Friend <stephaniefriend91@gmail.com>
2020-05-30 00:35:38 -0600

    Working on flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
-class UsersController < ApplicationController 
-
+class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
 
+  #flash message is not working
   def zip
     @user = User.find(params[:id])
-    @user.update(zipcode: user_params[:zipcode])
-    redirect_to "/profile/#{@user.id}"
+    update = @user.update(zipcode: user_params[:zipcode])
+    if user_params[:zipcode].nil?
+      flash[:error] = 'Invalid zipcode, please try again.'
+      redirect_to "/profile/#{@user.id}"
+    else
+      redirect_to "/profile/#{@user.id}"
+    end
   end
 
   def user_params
