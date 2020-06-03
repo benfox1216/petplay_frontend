4d93d2ae1ba65e3158b02cb93cf77a106456f6da
Ben Fox <benfox1216@gmail.com>
2020-06-03 03:11:20 -0600

    Fix bug on petplay show page, where the attending line was showing ids instead of names

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,8 +1,4 @@
 class Pet < ApplicationRecord
   validates_presence_of :name, :species, :breed, :size, :about_me
   belongs_to :user
-  
-  # def self.find_
-  #
-  # end
 end
--- app/models/pet_play.rb
+++ app/models/pet_play.rb
@@ -1,3 +1,10 @@
 class PetPlay < ApplicationRecord
   belongs_to :user
+  
+  # def get_names_from_ids
+  #   pp = pet_players.split(", ")
+  #
+  #   pp.each do
+  #   User.find(pet_players)
+  # end
 end
