1d40573b493d6b654e7ecaa5162184f9d9b7c0e9
Anthony Gomes <code@tonygom.es>
2020-06-04 22:45:52 -0600

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,6 @@ user_2.pets.create!(name: 'Durst', species: 'Dog', breed: 'Mixed', size: 'medium
 user_2.pets.create!(name: 'Little Devil', species: 'Cat', breed: 'Mixed', size: 'large', age: 7, about_me: 'Where am I?')
 
 #petplays
-user_1.petplays.create!(title: 'Big Dogs + Cats', location: '80215', pet_players: "#{user_1.image}", date: "10/02/2020", time: "10:00 AM")
-user_1.petplays.create!(title: 'Any and All', location: '80516', pet_players: "#{user_1.image}", date: "10/02/2020", time: "10:00 AM")
-
-user_2.petplays.create!(title: 'Cats Only!', location: '80215', pet_players: "#{user_1.image}, #{user_2.image}", date: "10/02/2020", time: "10:00 AM")
+user_1.petplays.create!(title: 'Big Dogs + Cats', location: 'Utah Park, 1800 S Peoria St, Aurora, CO 80202, USA', pet_players: "#{user_1.image}", date: "10/02/2020", time: "10:00 AM")
+user_1.petplays.create!(title: 'Any and All', location: 'Utah Park, 1800 S Peoria St, Aurora, CO 80202, USA', pet_players: "#{user_1.image}", date: "10/02/2020", time: "10:00 AM")
+user_2.petplays.create!(title: 'Cats Only!', location: 'Utah Park, 1800 S Peoria St, Aurora, CO 80202, USA', pet_players: "#{user_1.image}, #{user_2.image}", date: "10/02/2020", time: "10:00 AM")
